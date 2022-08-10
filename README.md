06e5a288c4bba570cb62d2551239d0d83c658ac8
Oladimeji Abdrahman Adebayo <67334984+rahmlad-aramide@users.noreply.github.com>
2022-08-10 20:23:41 +0100

    Update: Game building finished '

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Simon-game
-Simon Game Challenge from Full Stack web development course by Dr. Angela Yu
+## Simon-game v.2.0
+### Simon Game Challenge from Full Stack web development course by Dr. Angela Yu
+---
+- A brain building game and can be used to test your IQ 
+- I added a bit of other cool features to add spice to the game
+- This made me to call it an Updated Version the initial game `Version 2`
+- You can check it out here [Simon Game v.2]("https://")
--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-// 5. Game pattern
+// Initializing the variables
 let gamePattern = [];
 let userClickedPattern = [];
 let level = 0;
-
-// 3. Button colors array
-const buttonColours = ["red", "blue", "green", "yellow"]
+let randomChosenColour;
 let started = false;
 
-// 1. Next sequence function
-const nextSequence = () => {
-    // 2. Random Number
-    let randomNumber = Math.floor(Math.random() * 4);
-    console.log(randomNumber)
-    // 4. Random Chosen color
-    let randomChosenColour = buttonColours[randomNumber];
-    console.log(randomChosenColour);
-    animatePress(randomChosenColour);
-    // 6. add randome color to game pattern
-    gamePattern.push(randomChosenColour);
-    console.log(gamePattern);
-    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
-    level++;
-    $("h1").text(`Level ${level}`)
-}
+// Button colors array
+const buttonColours = ["red", "blue", "green", "yellow"]
+
+// Change the initial header text and start the game
 $(document).on("keypress", function(){
     if(!started){
         nextSequence();
@@ -30,28 +16,77 @@ $(document).on("keypress", function(){
         started = true;
     }
 });
-// $(document).on("keypress", alert("Pressed"));
 
-
-function playSound(name){
-    var audio = new Audio(`sounds/${name}.mp3`);
-    audio.play();
+// Next sequence function
+const nextSequence = () => {
+    userClickedPattern = [];
+    level++;
+    $("h1").text(`Level ${level}`)
+    // Random Number
+    let randomNumber = Math.floor(Math.random() * 4);
+    // Random Chosen color
+    randomChosenColour = buttonColours[randomNumber];
+    // add random color to game pattern
+    gamePattern.push(randomChosenColour);
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
 }
 
+// add effects to the clicked button
 $(".btn").click(function () {
     let userChosenColour = $(this).attr("id");
-    console.log(userChosenColour)
+    userClickedPattern.push(userChosenColour)
+
     playSound(userChosenColour)
     animatePress(userChosenColour);
-    userClickedPattern.push(userChosenColour)
-    console.log(userClickedPattern)
-    $(`#${userChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    checkAnswer(userClickedPattern.length-1)
 });
 
-function animatePress(item) {
+
+// Animate the button clicked
+const animatePress = (item) => {
     $(`#${item}`).addClass("pressed")
     setTimeout(function () {
         $(`#${item}`).removeClass("pressed");
         }, 100);
+};
 
+// Check the answer 
+const checkAnswer = (currentLevel) => {
+    if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
+        if(userClickedPattern.length == gamePattern.length){
+            setTimeout(() => {
+                nextSequence();
+            }, 1000);    
+        }
+    } else {
+        playSound("wrong");
+        $("body").addClass("game-over")
+        setTimeout(() => {
+            $("body").removeClass("game-over")
+        }, 200);
+        $("h1").text(`Game Over, Press any key to restart`)
+        startOver();
+    }
 }
+
+// Start the game over
+const startOver = () => {
+    level = 0;
+    gamePattern = [];
+    started = false;
+}
+
+// Play the button sound
+const playSound = (name) => {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+};
+// Play the background sound when the window has loaded
+const playBackgroundSound = (name) => {
+    let audio = new Audio(`sounds/${name}.mp3`);
+    audio.play();
+    audio.loop = true;
+};
+$(document).ready(playBackgroundSound("background"));
